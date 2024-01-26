130e05c2132b75a0965b3bf315d2a2bbb66133d9
Timothy Paek <tipaek@syr.edu>
2024-01-26 17:58:56 -0500

    initialize the backend, basic

---
+++ backend/moo-meditations/src/main/java/com/thegroup/moomeditations/MooMeditationsApplication.java
@@ -0,0 +1,13 @@
+package com.thegroup.moomeditations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooMeditationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooMeditationsApplication.class, args);
+	}
+
+}
---
+++ backend/moo-meditations/src/test/java/com/thegroup/moomeditations/MooMeditationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegroup.moomeditations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooMeditationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
