2bdcd5f3734d4bfda3f27ec2392840845ca89db0
tipaek <tax2farm@gmail.com>
2024-02-03 17:05:41 -0500

    update

---
+++ backend/src/test/java/com/thegroup/moomeditation/MooMeditationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegroup.moomeditation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooMeditationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
