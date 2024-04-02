89488d2ccc76e482b6ca7fe00b2c49459bf80970
Timothy Paek <106037728+tipaek@users.noreply.github.com>
2024-04-02 16:32:10 -0400

    main page layout

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,18 +1,36 @@
 <template>
-    <div id="play-button-section">
-        <h1 id="play-button">Play</h1>
+    <div class="overall-page">
+        <div class="left-layout">
+            <div class="username-section">
+                <h3 id="username">Username goes here</h3>
+            </div>
+            <div class="logout-section">
+                <h3 id="logout">Logout</h3>
+            </div>
+        </div>
+        <div class="main-layout">
+            <div class="main-text">
+                <h1>Welcome Back (username)</h1>
+            </div>
+            <div class="cow-section">
+                <!-- cow section goes here -->
+            </div>
+            <div class="metrics-section">
+                <div class="balance-section">
+                    <h3>Balance</h3>
+                </div>
+                <div class="minutes-section">
+                    <h3>Minutes</h3>
+                </div>
+            </div>
+        </div>
+        <div class="right-layout">
+            <div class="shop-section">
+                <h3 id="shop">Shop</h3>
+            </div>
+        </div>
     </div>
-    <div class="currency">this is the currency</div>
-    <div class="expandable-menu-section">
-        <div>this is the block</div>
-        <ul class="menu">
-            <li>Meditate</li>
-            <li>Store</li>
-        </ul>
-    </div>
-    <div class="cow">
-        this is the cow
-    </div>
+    \
 </template>
 <script>
     
@@ -22,26 +40,114 @@
     //}
 </script> 
 <style>
-    #play-button-section {
-        font-size: 5em;
-        text-align: center;
-        width: 50vw;
-        margin: 0 auto;
-        padding: 20px;
-        cursor: pointer; /* added code, push later in class - Indicates the element is clickable */
+    /* All STUFF */
+    * 
+    {
+        max-height:100vh;
+    }
+    .overall-page 
+    {
+        display:flex;
+        justify-content:space-apart;
+        width:100%;
+
+    }
+    .left-layout, .right-layout, .main-layout 
+    {
+        display:flex;
+        flex-direction:column;
+        height: 100vh;
+    }
+    .left-layout, .right-layout 
+    {
+        width:20vw;
+        padding: 3em 0 3em 0;
+    }
+    h3 
+    {
+        padding: 2em 0 2em 0;
+    }
+   
+
+    /* LEFT LAYOUT */
+    .left-layout 
+    {
+        justify-content:space-between;
+        background-color:grey;
+    }
+    .username-section 
+    {
+        background-color:black;
+        width: 70%;
+        height:13%;
+        text-align:center;
+        display:flex;
+        justify-content:center;
+        flex-direction:column;
+    }
+    .logout-section 
+    {
+        background-color:black;
+        width: 50%;
+        height:8%;
+        display:flex;
+        justify-content:center;
+        text-align:center;
+        flex-direction:column;
     }
 
-    .menu {
-        list-style: none;
-        display: none; /* added code, push later in class - Initially hides the menu */
+    /* RIGHT LAYOUT */
+    .right-layout 
+    {
+        justify-content:space-between;
+        background-color:red;
+    }
+    .shop-section 
+    {
+        background-color:black;
+        width:50%;
+        height:10%;
+        display:flex;
+        justify-content:center;
+        flex-direction:column;
     }
 
-    .menu li:hover {
-        background-color: lightgray; /* added code, push later in class - Adds hover effect on menu items */
+    /* MAIN SECTION */
+    .main-layout 
+    {
+        justify-content:space-evenly;
     }
+    .main-text 
+    {
+        background-color:pink;
+        width:100%;
+        height:15%;
+        display:flex;
+        justify-content:center;
+        flex-direction:column;
+    }
+    .metrics-section 
+    {
+        display: flex;
+        justify-content:space-apart;
+        width:120%;
+    }
+    .cow-section 
+    {
+        background-color:pink;
+        z-index:5;
+        width:110%;
+        height:50%;
+    }
+    .balance-section, .minutes-section 
+    {
+        background-color:black;
+        display:flex;
+        justify-content:center;
+        flex-direction:column;
+        width:50%;
+        height:60%;
+    }
+    
 
-    .currency, .cow {
-        text-align: center;
-        margin: 10px 0; /* added code, push later in class - Adds consistent styling for currency and cow sections */
-    }
 </style> 
